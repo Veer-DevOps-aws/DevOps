37232259bea9e61f5d945be949440c6424213367
Veer-DevOps-aws <veerdevops0796@gmail.com>
2025-04-07 22:18:35 +0530

    Update app.py 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Dockerized Python Application on Port 80!"
+    return "Hello,  Nextgen Team, this is Dockerized Python Application on Port 80!"
 
 if __name__ == "__main__":
     # Flask will listen on all IP addresses (0.0.0.0) and port 80
