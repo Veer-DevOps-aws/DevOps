6affdb1ee4dffae4676be79afdee7c734991b345
Veer-DevOps-aws <veerdevops0796@gmail.com>
2025-04-07 21:29:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Dockerized Python Application on Port 80!"
+
+if __name__ == "__main__":
+    # Flask will listen on all IP addresses (0.0.0.0) and port 80
+    app.run(host='0.0.0.0', port=80)
